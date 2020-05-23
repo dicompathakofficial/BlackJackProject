871870ada24f638d89b25c1dc09d6e012416f707
dicompathakofficial <dicompathakofficial2001@gmail.com>
2020-05-23 21:04:09 +0530

    Version 1.0

---
+++ hashmaps.py
@@ -0,0 +1,80 @@
+#HashMaps with Open Addressing Collision system
+class HashMap:
+
+    #while defining a hashmap No.1 thing is the size of the array.
+
+    def __init__(self, array_size):
+        self.array_size = array_size
+        self.array = [None for item in range(array_size)]
+
+    def hash(self, key, count_collision = 0):
+        key_bytes = key.encode() #converting the string into CS bytes
+        hash_code = sum(key_bytes) #suming up the bytes into an integer
+        return hash_code + count_collision
+
+    def compress(self, hash_code): #hash_code in compress() and hash_code in hash() are different variable for arguments
+        return hash_code % self.array_size # returing the remainder of the hash_code
+        #Note - hash() already exists in python
+        #Note - Since we are returning the remainder of hash_code
+        #with the array_size so no matter how big the collision is the index will be
+        #range(array_size)
+        #so using hash_map.compress(hash("")) is using python's hashing function
+        #and hash_map.compress(hash_map.hash("hi")) is our function
+
+    #Now, lets put [key, value pairs inside the array
+
+    def assign(self, key, value):
+        array_index = self.compress(self.hash(key))
+        current_index_value = self.array[array_index]
+
+        if current_index_value is None or current_index_value[0] == key: #we are storing an array inside an array
+            self.array[array_index] = [key, value]
+            return
+
+        #Incase of collisions
+
+        collision_counts = 1
+
+        while(current_index_value[0] != key):
+            new_array_index = self.compress(self.hash(key, collision_counts))
+            current_index_value = self.array[new_array_index] #assigning new value to the existing variable
+
+            if current_index_value is None or current_index_value[0] == key:
+                self.array[new_array_index] = [key, value]
+                return
+
+            collision_counts += 1 #so that the next time any collsion happens it will show a +plus 1 index value
+
+    # retireving th data related to the key
+
+    def retrieve(self, key):
+        array_index = self.compress(self.hash(key))
+        possible_value = self.array[array_index] #Same as the assign function
+
+        if possible_value is None:
+            return None #if there is nothing in the array with the given key we return None
+
+        elif possible_value[0] == key:
+            return possible_value[1] #it means that the key is found thus, we return the associated value
+
+        #Now, in case of a key, which created a collsion while assigning it so even though the index is same
+        #we will have to increment the index to find the value
+
+        collision_counts = 1
+
+        while(possible_value[0] != key):
+            new_array_index = self.compress(self.hash(key, collision_counts))
+            new_possible_value = self.array[new_array_index]
+
+            if new_possible_value is None:
+                return None
+
+            elif new_possible_value[0] == key:
+                return new_possible_value[1]
+
+            collision_counts += 1
+
+        return  # Same as the assign Function
+
+
+
---
+++ script.py
@@ -0,0 +1,106 @@
+import random
+from hashmaps import HashMap
+
+# INITIALIZING THE HASHMAP CLASS WITH THE ARRAY SIZE
+hash_map = HashMap(13)
+
+hash_map.assign("Ace", 1)  #("Ace", 11) calculated through algorithm
+hash_map.assign("Two", 2)
+hash_map.assign("Three", 3)
+hash_map.assign("Four", 4)
+hash_map.assign("Five", 5)
+hash_map.assign("Six", 6)
+hash_map.assign("Seven", 7)
+hash_map.assign("Eight", 8)
+hash_map.assign("Nine", 9)
+hash_map.assign("Ten", 10)
+hash_map.assign("Jack", 10)
+hash_map.assign("Queen", 10)
+hash_map.assign("King", 10)
+
+# LIST OF CARDS TO BE SELECTED OUT OF
+
+cards = ["Ace", "Twice", "Three", "Four",
+         "Five", "Six", "Seven", "Eight",
+         "Nine", "Ten", "Jack", "Queen",
+         "King"]
+
+game_on = True
+
+while game_on:
+
+    # GAME FUNCTIONS
+    def points(array):
+        total_points = 0
+        for card_name in array:
+            total_points += hash_map.retrieve(card_name)
+        return total_points
+
+    def print_cards(array_name, array):
+        print(array_name, array)
+
+    player_cards = []
+    dealer_cards = []
+
+    for _ in range(2):
+        player_cards.append(cards[random.randint(0, 12)])
+        dealer_cards.append(cards[random.randint(0, 12)])
+
+    print_cards("Player: ", player_cards)
+    print_cards("Dealer: ", dealer_cards[0])
+
+    win = "PLAYER WINS"
+    lost = "PLAYER LOST"
+    tied = "GAME TIED"
+
+    if "Ace" in player_cards:
+        if points(player_cards) + 10 == 21:
+            print("Player Wins, BLACKJACK!!")
+            break
+
+    user_input = input("Hit or pass")
+    if user_input == " H":
+        player_cards.append(cards[random.randint(0, 12)])
+        if points(player_cards) > 21:
+            print(lost)
+            print_cards("Player: ", player_cards)
+            print_cards("Dealer: ", dealer_cards)
+            break
+
+    if points(dealer_cards) < 17:
+        dealer_cards.append(cards[random.randint(1, 10)])
+        if points(dealer_cards) > 21 or points(player_cards) > points(dealer_cards):
+            print(win)
+            print_cards("Player: ", player_cards)
+            print_cards("Dealer: ", dealer_cards)
+            break
+        elif points(player_cards) == points(dealer_cards):
+            print(tied)
+            print_cards("Player: ", player_cards)
+            print_cards("Dealer: ", dealer_cards)
+            break
+        elif points(player_cards) < points(dealer_cards):
+            print(lost)
+            print_cards("Player: ", player_cards)
+            print_cards("Dealer: ", dealer_cards)
+            break
+
+    if points(player_cards) > points(dealer_cards):
+        print(win)
+        print_cards("Player: ", player_cards)
+        print_cards("Dealer: ", dealer_cards)
+        break
+    elif points(player_cards) == points(dealer_cards):
+        print(tied)
+        print_cards("Player: ", player_cards)
+        print_cards("Dealer: ", dealer_cards)
+        break
+
+    print(lost)
+    print_cards("Player: ", player_cards)
+    print_cards("Dealer: ", dealer_cards)
+
+
+
+
+    game_on = False
