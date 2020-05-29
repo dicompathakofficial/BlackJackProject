8a457f37db64d5c104d5625f0b2a53dac403a30b
dicompathakofficial <dicompathakofficial2001@gmail.com>
2020-05-29 14:23:54 +0530

    Initial Commit

--- script.py
+++ script.py
@@ -1,114 +1,88 @@
-# IMPORTING RANDOM MODULE AND THE HASHMAP DATA STRUCTURE
-# WHICH WE CREATED IN HASHMAPS.PY
+# Importing the random module
 import random
-from hashmaps import HashMap
 
-# INITIALIZING THE HASHMAP CLASS WITH THE ARRAY SIZE
-hash_map = HashMap(13)
+# Assigning the cards to their values
+card_deck = {'Ace': 1, 'Twice': 2, 'Thrice': 3,
+             'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7,
+             'Eight': 8, 'Nine': 9, 'Ten': 10,
+             'Jack': 10, 'Queen': 10, 'King': 10}
 
-hash_map.assign("Ace", 1)  #("Ace", 11) calculated through algorithm
-hash_map.assign("Two", 2)
-hash_map.assign("Three", 3)
-hash_map.assign("Four", 4)
-hash_map.assign("Five", 5)
-hash_map.assign("Six", 6)
-hash_map.assign("Seven", 7)
-hash_map.assign("Eight", 8)
-hash_map.assign("Nine", 9)
-hash_map.assign("Ten", 10)
-hash_map.assign("Jack", 10)
-hash_map.assign("Queen", 10)
-hash_map.assign("King", 10)
+# The cards list from which the cards will be distributed
+cards = ['Ace', 'Twice', 'Thrice',
+         'Four', 'Five', 'Six', 'Seven',
+         'Eight', 'Nine', 'Ten',
+         'Jack', 'Queen', 'King']
 
-# LIST OF CARDS TO BE SELECTED OUT OF
 
-cards = ["Ace", "Twice", "Three", "Four",
-         "Five", "Six", "Seven", "Eight",
-         "Nine", "Ten", "Jack", "Queen",
-         "King"]
+# Function for calculating the total points of the cards in hand
+def points(array):
+    total_points = 0
+    for card_name in array:
+        total_points += card_deck[card_name]
+    return total_points
 
-game_on = True
 
-while game_on:
+# viewing player and dealer cards
+def view_cards(player, dealer):
+    print(f"Player: {player},\n"
+          f"Dealer: {dealer}")
 
-    # GAME FUNCTIONS
-    def points(array):
-        total_points = 0
-        for card_name in array:
-            total_points += hash_map.retrieve(card_name)
-        return total_points
 
-    def print_cards(array_name, array):
-        print(array_name, array)
+# Player cards and Dealer cards
+player_cards = []
+dealer_cards = []
 
-    player_cards = []
-    dealer_cards = []
+# Distributing the two initial cards
+for _ in range(2):
+    player_cards.append(cards[random.randint(0, 12)])
+    dealer_cards.append(cards[random.randint(0, 12)])
 
-    for _ in range(2):
+# Showing the cards
+print(f"Player: {player_cards}")
+print(f"Dealer: {dealer_cards[0]}")
+
+# If the player has an Ace and a ten pointer [BLACKJACK]
+if "Ace" in player_cards:
+    if points(player_cards) + 10 == 21:
+        print("Player Wins, BLACKJACK!!\n")
+
+# If player doesn't have a blackjack
+else:
+    enter_input = input("Do you want to Hit or Pass(y/n):")
+
+    # the player can get cards while he presses y and hits
+    while enter_input == "y":
         player_cards.append(cards[random.randint(0, 12)])
-        dealer_cards.append(cards[random.randint(0, 12)])
+        print(player_cards)
 
-    print_cards("Player: ", player_cards)
-    print_cards("Dealer: ", dealer_cards[0])
-
-    win = "PLAYER WINS"
-    lost = "PLAYER LOST"
-    tied = "GAME TIED"
-
-    # CHECK FOR BLACKJACK
-    if "Ace" in player_cards:
-        if points(player_cards) + 10 == 21:
-            print("Player Wins, BLACKJACK!!")
-            break
-
-    # IF BLACKJACK DIDN'T HAPPEN
-    user_input = input("Hit or pass")
-    if user_input == " H":
-        player_cards.append(cards[random.randint(0, 12)])
+        # to check if the total points of his cards is more than 21
         if points(player_cards) > 21:
-            print(lost)
-            print_cards("Player: ", player_cards)
-            print_cards("Dealer: ", dealer_cards)
+            print("PLAYER LOST")
+            print(player_cards)
             break
+        enter_input = input("Do you want to Hit or Pass(y/n):")
 
-    # CHECK IF DEALER'S CARDS TOTAL TO LESS THAN 17
-    if points(dealer_cards) < 17:
-        dealer_cards.append(cards[random.randint(1, 10)])
+    # if the player presses p and passes
+    if enter_input == 'n':
+
+        # to check if dealer cards equal less than 17
+        if points(dealer_cards) < 17:
+            dealer_cards.append(cards[random.randint(0, 12)])
+
+        # to check if dealer points is more than 21 Or
+        # player points is more than the dealer
         if points(dealer_cards) > 21 or points(player_cards) > points(dealer_cards):
-            print(win)
-            print_cards("Player: ", player_cards)
-            print_cards("Dealer: ", dealer_cards)
-            break
+            print("PLAYER WINS")
+            view_cards(player_cards, dealer_cards)
+
+        # to check if game is tied and dealer and player has same points
         elif points(player_cards) == points(dealer_cards):
-            print(tied)
-            print_cards("Player: ", player_cards)
-            print_cards("Dealer: ", dealer_cards)
-            break
-        elif points(player_cards) < points(dealer_cards):
-            print(lost)
-            print_cards("Player: ", player_cards)
-            print_cards("Dealer: ", dealer_cards)
-            break
+            print("GAME TIED")
+            view_cards(player_cards, dealer_cards)
 
-    # IF DEALER'S CARDS ARE NOT LESS THAN 17
-    if points(player_cards) > points(dealer_cards):
-        print(win)
-        print_cards("Player: ", player_cards)
-        print_cards("Dealer: ", dealer_cards)
-        break
-    elif points(player_cards) == points(dealer_cards):
-        print(tied)
-        print_cards("Player: ", player_cards)
-        print_cards("Dealer: ", dealer_cards)
-        break
+        # to check if dealer has more points than player
+        else:
+            print("PLAYER LOST")
+            view_cards(player_cards, dealer_cards)
 
-    print(lost)
-    print_cards("Player: ", player_cards)
-    print_cards("Dealer: ", dealer_cards)
-
-
-
-
-    game_on = False
-
-# GAME DONE
+print("GAME COMPLETE")
